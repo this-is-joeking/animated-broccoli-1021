5a8562f161eed5e2d9d5e4a0eaee4db3e1cc1e27
Joe King <j.king92@gmail.com>
2023-01-13 10:58:29 -0700

    Removed unused methods from controllers

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,19 +1,6 @@
 class DoctorsController < ApplicationController
-  def index
-    @doctors = Doctor.all
-  end
-
   def show
     @doctor = Doctor.find(params[:id])
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
   end
 
   def update
@@ -22,14 +9,5 @@ class DoctorsController < ApplicationController
     doctor.patients.delete(patient)
 
     redirect_to doctor_path(doctor)
-  end
-
-  def destroy
-  end
-
-  private
-
-  def doctor_params
-    params
   end
 end
--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,30 +1,5 @@
 class HospitalsController < ApplicationController
-  def index
-    @hospitals = Hospital.all
-  end
-
   def show
     @hospital = Hospital.find(params[:id])
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-
-  def hospital_params
-    params
   end
 end
